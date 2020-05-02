868bef58f456e9b1d29f7e4e06f2b53d2b49290e
bipni <bipumirja@gmail.com>
2020-05-02 21:38:17 +0600

    container get changed. now object can through passing object type

--- src/telos/services/Container.py
+++ src/telos/services/Container.py
@@ -5,11 +5,18 @@ class Container:
         def singleton(self, key, value):
             self._container_dict[key] = value
 
-        def get(self, key):
-            if key not in self._container_dict:
-                return None
+        def get(self, obj):
+            if isinstance(obj, str):
+                if obj not in self._container_dict:
+                    return None
 
-            return self._container_dict[key]
+                return self._container_dict[obj]
+            else:
+                for key, value in self._container_dict.items():
+                    if isinstance(value, obj):
+                        return value
+                else:
+                    raise Exception("Object Not Found in Container")
 
     instance: __Container = None
 
