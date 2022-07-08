953d476c62de4bf3fc1466b69bff2d53e8b4a6c0
bipni <bipumirja@gmail.com>
2022-07-08 20:42:46 +0600

    plugins added

---
+++ src/telos/ui/src/plugins/authentication/authentication.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Authentication extends React.Component {
+
+    render() {
+        return <h1>This is a authentication plugins!</h1>;
+    }
+}
+
+export default Authentication;
