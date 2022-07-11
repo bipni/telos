693a06ff056e6f22696350a97b365e9cf41043b1
bipni <bipumirja@gmail.com>
2022-07-11 21:25:58 +0600

    registration added

--- src/telos/ui/src/plugins/authentication/components/login.js
+++ src/telos/ui/src/plugins/authentication/components/login.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import '../css/Login.css';
+import '../styles/Login.css';
 
 
 function Login() {
--- src/telos/ui/src/plugins/authentication/components/registration.js
+++ src/telos/ui/src/plugins/authentication/components/registration.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+
+import '../styles/Login.css';
 
 
 function Registration() {
 
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const renderForm = (
+        <div className="login-form">
+            <form>
+                <div className="input-container">
+                    <label>Name </label>
+                    <input type="text" name="name" required />
+                </div>
+
+                <div className="input-container">
+                    <label>Email </label>
+                    <input type="text" name="email" required />
+                </div>
+
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                </div>
+
+                <div className="input-container">
+                    <label>Passsword </label>
+                    <input type="password" name="pass" required />
+                </div>
+
+                <div className="input-container">
+                    <label>Re-enter Passsword </label>
+                    <input type="password" name="repass" required />
+                </div>
+
+                <div className="button-container">
+                    <input type="submit" />
+                </div>
+            </form>
+        </div>
+    );
+
     return (
-        <h1>Registration Page</h1>
+        <div className="app">
+            <div className="login-form">
+                <div className="title">Registration</div>
+                {isSubmitted ? <div>Registration is successfull.</div> : renderForm}
+            </div>
+        </div>
     );
 }
 
